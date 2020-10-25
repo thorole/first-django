27eea81af09382a0238245ea44dd3d293da2a9ab
Thor-Ole Øby <punkthor@gmail.com>
2020-10-25 16:46:04 +0100

    Added else if statement to check for development for secret key.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+if development:
+    SECRET_KEY = env("SECRET_KEY")
+else:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
